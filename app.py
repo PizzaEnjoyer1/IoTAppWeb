a923a0aeda73c81a93821d65d80ba69054cdece2
PizzaEnjoyer1 <133069709+PizzaEnjoyer1@users.noreply.github.com>
2024-09-30 20:09:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ if st.button('Enviar valor de ángulo al servo'):
         <div style="position: relative; width: 200px; height: 200px;">
             <div style="position: absolute; width: 100%; height: 100%; border: 2px solid black; border-radius: 50%;"></div>
             <div style="position: absolute; width: 2px; height: 90px; background: red; 
-                transform-origin: bottom center; 
+                transform-origin: bottom; 
                 transform: rotate({current_angle}deg); 
                 bottom: 50%; left: 50%; margin-left: -1px; margin-bottom: -90px;"></div>
         </div>
